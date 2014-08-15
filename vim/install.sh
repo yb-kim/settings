a6f76a16f24501732d4e0bc5c2ddd0bf0fbdfc28
Youngbin Kim <yb.kim@yonsei.ac.kr>
2014-08-15 18:47:04 +0900

    Add pathogen and vim-python-pep8-indent

--- vim/install.sh
+++ vim/install.sh
@@ -1,1 +1,19 @@
 cp .vimrc ~/
+
+echo "Check if pathogen installed..."
+
+PATHOGEN_PATH=.vim/autoload/pathogen.vim
+if [ -e ~/$PATHOGEN_PATH ]; then
+    echo "Pathogen already installed"
+else
+    echo "Pathogen not found. Install pathogen..."
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    cp $PATHOGEN_PATH ~/$PATHOGEN_PATH
+fi
+
+echo "Installing vim-python-pep8-indent..."
+if [ -e ~/.vim/bundle/vim-python-pep8-indent ]; then
+    echo "Already installed"
+else
+    cp -r .vim/bundle/vim-python-pep8-indent ~/.vim/bundle
+fi
